ea11247a8fb8e5668e565f01ea0299d60a345235
CodingPluto <95600256+CodingPluto@users.noreply.github.com>
2023-11-29 15:44:35 +1300

    Fixed image stretching, can now create a texture at a place on the screen using regular coordiantes in the new load_texture_and_vertices() function.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 //#include <SDL_image.h> No longer using SDL_image as I couldn't get image loading to work with it. SOIL2 is being used instead.
 
 GLuint load_vertices(GLfloat old_vertices[32]);
-
+std::pair<GLuint, GLuint>load_texture_and_vertices(const char imagePath[], int raw_x, int raw_y);
 GLuint load_texture(const char imagePath[]);
 std::vector <GLuint*> vbo_cache;
 
@@ -52,27 +52,15 @@ int main(int argc, char *argv[])
     Shader our_shader( "core.vs", "core.frag" );
 
     /////////////////////// Vertex Data //////////////////////
-    GLfloat vertices[] = {
-        // Positions          // Colors           // Texture Coords
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
-    };
-    GLuint vao = load_vertices(vertices);
-    GLfloat vertices2[] = {
-        // Positions          // Colors           // Texture Coords
-        -0.5f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-        -0.5f, 0.4f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-        -1.0f, 0.4f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
-    };
-    GLuint vao2 = load_vertices(vertices2);
-
+    //auto texture_png = load_texture("mower.png");
+    auto pair = load_texture_and_vertices("test_image.jpg", 200, 200);
+    GLuint vao = pair.first;
+    GLuint texture_jpg = pair.second;
+    pair = load_texture_and_vertices("mower.png", 300, 300);
+    GLuint vao2 = pair.first;
+    GLuint texture_png = pair.second;
 
     /////////////////////// Loading Texture //////////////////////
-    auto texture_png= load_texture("mower.png");
-    auto texture_jpg = load_texture("test_image.jpg");
 
     //////////////////////////////////////////////////////////////////////////
     //////////////////////////////// Game Loop ///////////////////////////////
@@ -132,7 +120,80 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
+std::pair<GLuint,GLuint>load_texture_and_vertices(const char imagePath[] , int raw_x, int raw_y)
+{
+  // Relative positions now work!!
+  GLuint texture;
+  int width, height;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // texture parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // texture filtering
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  unsigned char* image = SOIL_load_image(imagePath, &width, &height, 0, SOIL_LOAD_RGBA);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  // Free image (texture kept)
+  SOIL_free_image_data(image);
+  glBindTexture(GL_TEXTURE_2D, 0);
+  float adj_x = 0;
+  float adj_y = 0;
+  if (raw_x != 0) adj_x = (2.0f * ((float)raw_x / WIDTH)) - 1.0f;
+  if (raw_y != 0) adj_y = -((2.0f * ((float)raw_y / HEIGHT)) - 1.0f);
+  float adj_w = 0;
+  float adj_h = 0;
+  if (width != 0) adj_w = (2.0f * ((float)width / WIDTH));
+  if (height != 0) adj_h = -((2.0f * ((float)height / HEIGHT)));
 
+  GLfloat vertices[] = {
+    // Positions          // Colors           // Texture Coords
+     adj_x + adj_w,  adj_y,        0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+     adj_x + adj_w , adj_y + adj_h, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+     adj_x,         adj_y + adj_h, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+     adj_x,         adj_y,        0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+  };
+
+  GLuint indices[] =
+  {
+      0, 1, 3, // First Triangle
+      1, 2, 3  // Second Triangle
+  };
+  /////////////////////// Creatng Buffers //////////////////////
+  GLuint vbo, vao, ebo;
+  glGenVertexArrays(1, &vao);
+  glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
+  // vao
+  glBindVertexArray(vao);
+  // vbo
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // ebo
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  /////////////////////// Setting Attributes //////////////////////
+  // Position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+  glEnableVertexAttribArray(0);
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+  // Texture Coordinate attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(2);
+  glBindVertexArray(0); // Unbind VAO
+
+  vbo_cache.emplace_back(&vbo);
+  vao_cache.emplace_back(&vao);
+  ebo_cache.emplace_back(&ebo);
+
+
+  return std::pair<GLuint, GLuint>(texture, vao);
+}
 
 
 GLuint load_texture(const char imagePath[])
