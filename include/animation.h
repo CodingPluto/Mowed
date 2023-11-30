b11c8ecb7aeb517190871715a1697a7b90564b39
Cerberus <hugh.bill@icloud.com>
2023-11-30 15:15:14 +1300

    animation class

--- include/animation.h
+++ include/animation.h
@@ -3,20 +3,23 @@
 
 #include <iostream>
 #include <vector>
-#include <SDL.h>
+#include <SOIL2.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
 
 class  animation
 {
 public:
-	animation();
-	GetFrame();
-	SetNextAnmation
+	animation(std::string name);
+	GLint GetFrame();
+	void Setstate(int state);
 
 private:
-	std::vector<std::vector<SDL_Texture>> animations_;
-	int animation_;
+	std::vector<std::vector<std::pair<GLuint, GLuint>(texture, vao)>> animations_;
+	std::pair<int> animation_frame_;
 	int animation_fps_:
 	int next_animation;
+	int last_timestep = 0;
 	
 };
 
--- src/animation.cc
+++ src/animation.cc
@@ -1,11 +1,31 @@
 #include "animation.h"
 
-animation::animation()
+#include <SDL.h>
+#include <render.h>
+
+animation::animation(std::string name)
 {
+    std::string path = "../assets/" + name + /;
+    int index_pointer = 0;
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+        std::string animation_path = entry.path();
+        index_pointer += 1;
+        for (const auto& animation_entry : std::filesystem::directory_iterator(animation_path)) {
+            auto frame = load_texture_ex(animation_entry.path());
+            std::pair<GLuint, GLuint>(texture, vao)> buffer;
+            buffer.first = frame.first;
+            buffer.second = load_vertices_ex(300, 300, frame.second.first, frame.second.second);
+            animation_[index_pointer].emplace_back(buffer); 
+        }
+    }
 }
 
-animation::GetFrame()
+GLuint animation::GetFrame()
 {
+
 }
 
+void animation::Setstate(int state)
+{
+}
 
